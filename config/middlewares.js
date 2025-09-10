a4552faf8931ee6ca2741c624227d574cbf9bbc5
Gamal Abdelnaser <136737555+Gamal-abdelnaser@users.noreply.github.com>
2025-09-10 22:47:45 +0300

    update: latest changes

--- config/middlewares.js
+++ config/middlewares.js
@@ -5,7 +5,7 @@ module.exports = [
     name: 'strapi::cors',
     config: {
       enabled: true,
-      origin: ['http://localhost:5173'], // هنا لينك الفرونت
+      origin: ['https://modivo.vercel.app'], // هنا لينك الفرونت
       methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     },
   },
